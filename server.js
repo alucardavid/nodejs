9d9a2ca1f55d5b2dd395a30af588a11d38217411
David Pereira <david.pereira@onofre.com.br>
2019-04-11 11:44:55 -0300

    Sharing Request and Response

--- server.js
+++ server.js
@@ -1,52 +1,21 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
 
-function send404(response) {
-    response.writeHead(404, {'Content-Type' : 'text/plain'});
-    response.write('Error 404: Resource not found.');
-    response.end();
+var connect = require('connect');
+var util = require('util');
+
+function logit(req, res, next) {
+    util.log(`Request received: ${req.method}, ${req.url}`);
+    next();
 }
 
-var mimeLookup = {
-    '.js': 'application/javascript',
-    '.html': 'text/html'
+let echo = {
+    handle: (req, res, next) => {
+        req.pipe(res);
+    }
 }
 
-var server = http.createServer((req, res) => {
-    if (req.method == 'GET') {
+var app = connect()
+            .use('/echo', echo)
+            .use((req, res) => {res.end('Wassup!')})
+            .listen(3000);
 
-        var fileUrl;
-
-        if (req.url == '/') {
-            fileUrl = '/index.html';
-        }
-        else {
-            fileUrl = req.url;
-        }
-        
-        var filepath = path.resolve('./public' + fileUrl);
-        var fileExt = path.extname(filepath);
-        var mimeType = mimeLookup[fileExt];
-
-        if (!mimeType) {
-            send404(res);
-            return;
-        }
-        
-        fs.exists(filepath, exists => {
-            if (!exists) {
-                send404(res);
-                return;
-            }
-            
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            fs.createReadStream('./public/index.html').pipe(res);
-        });
-    }
-    else {
-        send404(res);
-    }
-}).listen(3000);
-
-console.log('server running on port 3000');
+console.log('server running on port 3000')
