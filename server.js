4e76b0b221d4835516da70189d5a9bfb6269e62b
David Pereira <david.pereira@onofre.com.br>
2019-04-17 18:26:07 -0300

    Init Express

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
+var express = require('express'),
+    http = require('http');
 
-var https = require('https');
-var fs = require('fs');
-var app = require('connect')();
+// Create an express application
+express()
+    .use((req, res, next) => {
+        res.end('hello express!');
+    }).listen(3000);
 
-var options = {
-    key: fs.readFileSync('./key.pem'),
-    cert: fs.readFileSync('./cert.pem')
-};
-
-https.createServer(options, (req, res) => {
-    res.end('secure!');
-}).listen(443);
-
-var http = require('http');
-
-http.createServer((req, res) => {
-    res.writeHead(301, {Location: `https://${req.headers['host']+req.url}`});
-    res.end();
-}).listen(80);
-
-console.log('Servidor rodando na porta 3000');
-
+console.log('Server running in port 3000');
