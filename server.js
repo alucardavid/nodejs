09e7301ad3e0eea6eebd29b27006f2f7f4eb3b15
David Pereira <david.pereira@onofre.com.br>
2019-04-17 18:11:25 -0300

    Basic using Https

--- server.js
+++ server.js
@@ -1,55 +1,23 @@
 
-var connect = require('connect');
-var util = require('util');
+var https = require('https');
+var fs = require('fs');
+var app = require('connect')();
 
-// function logit(req, res, next) {
-//     util.log(`Request received: ${req.method}, ${req.url}`);
-//     next();
-// }
+var options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
+};
 
-// function auth(req, res, next){
+https.createServer(options, (req, res) => {
+    res.end('secure!');
+}).listen(443);
 
-//     var authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         send401(res);
-//         return;
-//     }
-    
-//     var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-//     var user = auth[0];
-//     var pass = auth[1];
-    
-//     if (user == 'foo' && pass == 'bar') {
-//         console.log(req);
-        
-//         next();
-//     }
-//     else {
-//         send401(res);
-//     }
+var http = require('http');
 
-//     function send401(res) {
-//         res.writeHead(401, {'WWW-Authenticate': 'Basic'});
-//         res.end();
-//     }
-// }
+http.createServer((req, res) => {
+    res.writeHead(301, {Location: `https://${req.headers['host']+req.url}`});
+    res.end();
+}).listen(80);
 
-// let echo = {
-//     handle: (req, res, next) => {
-//         req.pipe(res);
-//     }
-// }
+console.log('Servidor rodando na porta 3000');
 
-var app = connect()
-            .use((req, res, next) => { next(new Error('Big bad error details')); })
-            .use((req, res, next) => { res.end('I will never ge called'); })
-            .use((err, req, res, next) => {
-                console.log('Error handled:', err.message);
-                console.log('Stacktrace:', err.stack);
-                
-                res.writeHead(500);
-                res.end('Unable to process the request');
-            })
-            .listen(3000);
-
-console.log('server running on port 3000');
