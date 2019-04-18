d003201d627023d2c468f4ee26b00051b63c0b40
David Pereira <david.pereira@onofre.com.br>
2019-04-18 11:56:56 -0300

    Serving static pages

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express = require('express'),
-    http = require('http');
+var express = require('express');
+    
 
 // Create an express application
 express()
-    .use((req, res, next) => {
-        res.end('hello express!');
-    }).listen(3000);
+    .use(express.static(`${__dirname}/public`)).listen(3000);
 
 console.log('Server running in port 3000');
