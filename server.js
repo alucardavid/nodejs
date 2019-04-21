959ecbd026f5951165b8750c990a9d85f5fc38cb
David Pereira <d_pereira@outlook.com.br>
2019-04-21 20:42:53 -0300

    Update and delete mongodb

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
 
-var demoPerson = { name: 'John', lasname: 'Smith' };
+var demoPerson = { name: 'John', lastname: 'Smith' };
 var findKey = { name: 'John' };
 var url = 'mongodb://127.0.0.1:27017';
 var dbName = 'demo';
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url, { useNewUrlParser: true}, (err, client) => {
     if (err) throw err;
     console.log('Successfully connected');
 
@@ -13,18 +13,40 @@ MongoClient.connect(url, (err, client) => {
     var collection = db.collection('people');
 
     collection.insertOne(demoPerson, (err, docs) => {
-
         console.log(`Inserted ${JSON.stringify(docs.ops[0])}`);
         console.log(`ID: ${demoPerson._id}`);
 
-        collection.find(findKey).toArray((err, results) => {
-            console.log(`Found results: ${JSON.stringify(results)}`);
-
-            collection.deleteOne(findKey, (err, results) => {
-                console.log('Deleted person');
-                
-                client.close();
-            });
-        });
     });
+
+    collection.updateOne({ _id: demoPerson._id }, { $set: { lastname: 'Pereira' }}, (err, result) => {
+        if (err) {
+            console.log('Was not possible to update');
+        }
+        else {
+            console.log('Updated');
+            
+        }
+    });
+
+    // collection.find({}).toArray((err, docs) => {
+    //     console.log(`Found results: ${JSON.stringify(docs)}`);
+
+    //     // collection.deleteOne(findKey, (err, results) => {
+    //     //     console.log('Deleted person');
+            
+    //     //     client.close();
+    //     // });
+    // });
+
+    // collection.deleteMany({name: 'John'}, (err, result) => {
+    //     if (err) {
+    //         console.log('Was not possible delete theses peoples');
+    //     }
+    //     else{
+    //         console.log('Deletados');
+            
+    //     }
+    // });
+    
+    client.close();
 });
