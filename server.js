2d0106cfdbded737d9d9f50575ecc563a40aff07
David Pereira <d_pereira@outlook.com.br>
2019-04-27 13:33:23 -0300

    Generators

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
-var express = require('express'),
-    Q = require('q');
+// var express = require('express'),
+//     Q = require('q');
 
-var loadItem = Q.nbind((id, cb) => {
-    setTimeout(() => {
-        cb(null, { id: id});
-    }, 500);
-});
+function* generator() {
+    var bar = yield 'foo';
+    console.log(bar);
+}
 
-Q.all([loadItem(1), loadItem(2)])
-    .then(items => {
-        console.log(`Items: ${JSON.stringify(items)}`);
-    })
-    .catch(reason => {
-        console.log(reason);
-    });
+function* generator2() {
+    try {
+        yield 'foo';
+    } catch (err) {
+        console.log(err.message);
+        
+    }
+}
+
+var iterator = generator();
+// Start execution till we get first yield value
+var foo = iterator.next();
+console.log(foo.value);
+// Resume exection injecting bar
+var nextThing = iterator.next('bar');
+console.log(iterator.next());
+
+var errIterator = generator2();
+
+var foo = errIterator.next();
+console.log(foo.value);
+var nextThing = errIterator.throw(new Error('bar'));
 
 
-var app = express()
-    .use(express.static(`${__dirname}/public`))
-    .listen(3000);
 
-console.log('Server running in port 3000');
+// var app = express()
+//     .use(express.static(`${__dirname}/public`))
+//     .listen(3000);
+
+// console.log('Server running in port 3000');
 
