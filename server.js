52a59b9ea5edb48519bcb606a58181ed7fc6aa9b
David Pereira <d_pereira@outlook.com.br>
2019-04-28 16:03:05 -0300

    Testing

--- server.js
+++ server.js
@@ -1,30 +1,68 @@
-// var express = require('express'),
-//     Q = require('q');
+var assert = require('assert');
+var List = require('./assert/list');
 
-function foo() {
-    var stack = new Error('trace at foo error').stack;
-    
-    // Type one
-    console.trace('trace at foo');
+var list = new List();
 
-    // Type two
-    console.log(stack);
+console.log('Testing list count');
+assert.equal(list.count(), 0);
 
-    // Executi[on continues
-    console.log('Stack trace printed');
-    
-}
+console.log('Testing list add');
+list.add({
+    id: '1',
+    value: 'some value'
+});
+assert.equal(list.count(), 1);
 
-function bar(){
-    foo();
-}
+console.log('Testing list clear');
+list.clear();
+assert.equal(list.count(), 0);
 
-bar();
+console.log('Testing list getIds');
+list.add({
+    id: '2',
+    value: 'David'
+});
+assert.equal(list.getIds()[0], '2');
+list.clear();
+
+console.log('Testing list remove');
+list.add({
+    id: '1',
+    value: 'David'
+});
+list.remove('1');
+assert.equal(list.count(), 0);
+
+console.log('Testing list get');
+list.add({
+    id: '1',
+    value: 'David'
+});
+assert.equal(list.get('1').value, 'David');
+list.clear();
+
+console.log('Testing list.add throws an error on invalid value');
+assert.throws(function() {
+    list.add({
+        value: 'David'
+    })
+},
+function (err) {
+    return (err instanceof Error) && (err.message == 'item must have id');
+});
 
 
-// var app = express()
-//     .use(express.static(`${__dirname}/public`))
-//     .listen(3000);
 
-// console.log('Server running in port 3000');
+
+
+
+
+
+
+
+
+
+
+
+
 
