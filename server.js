45d0f3610ec284f77ead069d0e642d41b564281a
David Pereira <david.pereira@onofre.com.br>
2019-04-29 12:08:22 -0300

    Mocha

--- server.js
+++ server.js
@@ -1,56 +1,15 @@
 var assert = require('assert');
-var List = require('./assert/list');
 
-var list = new List();
+describe('our test suite', function() {
+    
+    it('should pass this tes', function() {
+        assert.equal(1, 1, '1 should be equal to 1');
+    });
 
-console.log('Testing list count');
-assert.equal(list.count(), 0);
-
-console.log('Testing list add');
-list.add({
-    id: '1',
-    value: 'some value'
+    it('should fail this test', function() {
+        assert.equal(1, 0, '1 should be equal to 0');
+    });
 });
-assert.equal(list.count(), 1);
-
-console.log('Testing list clear');
-list.clear();
-assert.equal(list.count(), 0);
-
-console.log('Testing list getIds');
-list.add({
-    id: '2',
-    value: 'David'
-});
-assert.equal(list.getIds()[0], '2');
-list.clear();
-
-console.log('Testing list remove');
-list.add({
-    id: '1',
-    value: 'David'
-});
-list.remove('1');
-assert.equal(list.count(), 0);
-
-console.log('Testing list get');
-list.add({
-    id: '1',
-    value: 'David'
-});
-assert.equal(list.get('1').value, 'David');
-list.clear();
-
-console.log('Testing list.add throws an error on invalid value');
-assert.throws(function() {
-    list.add({
-        value: 'David'
-    })
-},
-function (err) {
-    return (err instanceof Error) && (err.message == 'item must have id');
-});
-
 
 
 
