6c82ee4342cd9e354f59847f723454127adb029c
David Pereira <david.pereira@onofre.com.br>
2019-04-29 14:53:59 -0300

    Testing Promises

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 var assert = require('assert');
+var Q = require('q');
 
 describe('our test suite', () => {
 
-    it('this should pass', done => {
-        setTimeout(() => {
-            done();
-        }, 500);
+    it('this should pass', () => {
+        return Q.when('pass');
     });
 
-    it('this should fail', done => {
-        setTimeout(() => {
-            done(new Error('fail'));
-        }, 500);
+    it('this should fail', () => {
+        return Q.reject(new Error('fail'));
     });
 });
 
